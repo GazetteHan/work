3997060b806e2d8818ecb8ff8b4bd1408e209518
GazetteHan <sokolo2024@mail.ru>
2023-02-06 15:47:43 +0300

    asdduj

--- 20 tasks/task_2.php
+++ 20 tasks/task_2.php
@@ -21,7 +21,7 @@
         <main id="js-page-content" role="main" class="page-content">
 
             <div class="col-md-6">
-                <div id="panel-1" class="panel"> sad
+                <div id="panel-1" class="panel">
                     <div class="panel-hdr">
                         <h2>
                             Задание
