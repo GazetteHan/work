72903d8da6d87fa7482e479a457813bef54fed12
Chuzhmakov Andrey <>
2023-03-01 12:20:08 +0300

    Add class Session

--- cleanOOP/index.php
+++ cleanOOP/index.php
@@ -1,7 +1,11 @@
 <?php
-
+session_start();
 include_once "Config.php";
 include_once "Database.php";
+include_once "Validate.php";
+include_once "Input.php";
+include_once "Token.php";
+include_once "Session.php";
 
 //Database::getInstance()->insert('users', [
 //	'username' => 'Marlin',
@@ -13,31 +17,95 @@ include_once "Database.php";
 //]);
 
 $GLOBALS["config"] = [
-    'mysql' => [
-        "host"      => "localhost",
-        "username"  => "root",
-        "password"  => "",
-        "database"  => "marlin_cleanoop",
-        "something" => [
-            "no" => [
-                "foo" => [
-                    "bar" => "baz"
-                ]
-            ]
-        ]
-    ]
+		'mysql'   => [
+				"host"      => "localhost",
+				"username"  => "root",
+				"password"  => "",
+				"database"  => "test",
+				"something" => [
+						"no" => [
+								"foo" => [
+										"bar" => "baz"
+								]
+						]
+				]
+		],
+		'session' => [
+				'token_name' => 'token',
+		]
 ];
 
 
-$users = Database::getInstance()->get('users', ['username', '=', 'Marlin']);
+$users = Database::getInstance()->get('users', ['id', '=', '1']);
 //Database::getInstance()->delete('users', ['username', '=', 'name2']);
 
 
-if ($users->error()) {
-    echo "This Error";
-} else {
-    foreach ($users->result() as $user) {
-        echo $user["id"] . ". " . $user["username"] . "<br>";
-        //		echo $user . "<br>";
-    }
+//if ($users->error()) {
+//	echo "This Error";
+//} else {
+//	foreach ($users->result() as $user) {
+//		echo $user["id"] . ". " . $user["username"] . "<br>";
+//		//		echo $user . "<br>";
+//	}
+//}
+
+
+if (Input::exists()) {
+	if (Token::check(Input::get('token'))) {
+		$validate = new Validate();
+		$validation = $validate->check($_POST, [
+				'username'       => [
+						'required' => true,
+						'min'      => 2,
+						'max'      => 15,
+						'unique'   => 'users'
+				],
+				'password'       => [
+						'required' => true,
+						'min'      => 3,
+				],
+				'password_again' => [
+						'required' => true,
+						'matches'  => 'password'
+				],
+		]);
+
+		//	var_dump($validation->errors());
+
+		if ($validation->passed()) {
+			echo 'passed';
+		} else {
+			foreach ($validation->errors() as $error) {
+				echo $error . '<br>';
+			}
+		}
+	}
+
 }
+
+?>
+
+
+<form action="" method="post">
+	<div class="field">
+		<label for="username">Username</label>
+		<input type="text" name="username" value="<?php echo Input::get('username'); ?>">
+	</div>
+	<div class="field">
+		<label for="password">Password</label>
+		<input type="text" name="password">
+	</div>
+
+	<div class="field">
+		<label for="password_again">Password again</label>
+		<input type="text" name="password_again">
+	</div>
+    <input type="hidden" name="token" value="<?php echo Token::generate() ?>">
+
+    <div class="field">
+        <button type="submit">Submit</button>
+
+
+
+	</div>
+</form>
